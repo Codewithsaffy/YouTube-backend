9b97f0c21c710d7dd2bd7c34a41c59c02efb1d1c
codewithsaffy <codewithsaffy@gmail.com>
2024-08-23 01:45:45 +0500

    add

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -428,6 +428,61 @@ const getUserChannelProfile = asyncHandler(async (req: any, res: Response) => {
       new ApiResponse(200, "channel details fetched successfully", channel[0]),
     );
 });
+
+//! ==================
+// * fetch watch history
+//! ==================
+const getUserWatchHistory = asyncHandler(async (req: any, res: Response) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req?.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              $first: "$owner",
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "fetched watched history suuccessfully",
+        user[0].watchHistory,
+      ),
+    );
+});
 export {
   registerUser,
   loginUser,
@@ -439,4 +494,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getUserWatchHistory,
 };
--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changedUserPassward,
+  getCurrentUser,
+  getUserChannelProfile,
+  getUserWatchHistory,
   loginUser,
   logoutUser,
   refreshAsseccTockenUser,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetail,
 } from "../controllers/users.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,6 +36,20 @@ router.route("/login").post(loginUser);
 // secured route
 
 router.route("/logout").post(verifyJWT, logoutUser);
-
 router.route("/refresh-token").post(refreshAsseccTockenUser);
+router.route("/change-passward").post(verifyJWT, changedUserPassward);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateUserDetail);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
+
 export default router;
